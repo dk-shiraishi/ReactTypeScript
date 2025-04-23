c4207d8fb7235d3cbb894a3ba7fbca477d558c9b
dk-shiraishi <shiraishi@daikoku.co.jp>
2025-04-24 04:16:21 +0900

    lesson-done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import './App.css'
 import { Practice1 } from './practices/Practice1'
+import { Practice2 } from './practices/Practice2'
+import { Practice3 } from './practices/Practice3'
+import { Practice4 } from './practices/Practice4'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +11,9 @@ function App() {
   return (
     <>
     <Practice1 />
+    <Practice2 />
+    <Practice3 />
+    <Practice4 />
     </>
   )
 }
